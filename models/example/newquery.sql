aab508d3fdca5d3d1b2f24d3a0922a1e36903f18
Sukru Bal <balbilis@gmail.com>
2024-09-23 20:07:32 +0000

    added newquery

---
+++ models/example/newquery.sql
@@ -0,0 +1,6 @@
+select provider_id as inpatient_provider_id,
+provider_state as inpatient_provider_state,
+round(sum(total_discharges),2) as inpatient_sum_total_discharges,
+round(sum(average_covered_charges),2) as inpatient_sum_average_covered_charges from
+bigquery-public-data.cms_medicare.inpatient_charges_2011 
+group by 1,2
